404635045148baf5d796ccfc6e41c585e4c3263a
Gregory Oschwald <oschwald@gmail.com>
2026-06-13 18:51:59 -0700

    Accept public Metadata module in tests

--- tests/maxmind/reader_test.py
+++ tests/maxmind/reader_test.py
@@ -456,7 +456,7 @@ class BaseTestReader(unittest.TestCase):
         metadata = reader.metadata()
         with self.assertRaisesRegex(
             AttributeError,
-            r"'(maxminddb(_rust)?\.extension\.)?Metadata' object has no attribute 'blah'",
+            r"'((maxminddb(_rust)?\.)?(extension\.)?)?Metadata' object has no attribute 'blah'",
         ):
             metadata.blah  # type:  ignore[attr-defined]  # noqa: B018
         reader.close()
